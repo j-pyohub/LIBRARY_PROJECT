b3c2855bb2608409955772a2c3ab45f7d6dfefc4
fuu-1 <moham18741@gmail.com>
2025-11-20 17:42:27 +0900

    feat : add getManagerTest

---
+++ src/main/java/com/oopsw/erp_project/repository/Manager.java
@@ -0,0 +1,32 @@
+package com.oopsw.erp_project.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "manager")
+public class Manager {
+    @Id
+    private String managerId;
+    private String pw;
+    private String email;
+    private String managerName;
+    private String phoneNumber;
+    @CreationTimestamp
+    private Timestamp inDate;
+    private Timestamp delDate;
+    private String role;
+
+}
---
+++ src/test/java/com/oopsw/erp_project/repository/ManagerRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.oopsw.erp_project.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManagerRepositoryTest {
+    @Autowired
+    ManagerRepository managerRepository;
+
+
+    @Test
+    public void getManager(){
+        System.out.println(managerRepository.findAll());
+    }
+
+
+}
