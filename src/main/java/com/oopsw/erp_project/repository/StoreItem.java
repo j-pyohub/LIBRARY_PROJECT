04670d2ca2d67f20911f6767c8bbd6a154ad321f
moonycode <jmoony0903@gmail.com>
2025-11-20 18:06:08 +0900

    feat : add StoreItem Entity, Repository

---
+++ src/main/java/com/oopsw/erp_project/repository/StoreItem.java
@@ -0,0 +1,22 @@
+
+package com.oopsw.erp_project.repository;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StoreItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long storeItemNo;
+
+    private Long itemNo;
+    private Long storeNo;
+    private Integer managerLimit;
+    private Integer storeLimit;
+}
