8b80886b35de843f89fcb0b13675b46815f35d4f
fuu-1 <moham18741@gmail.com>
2025-11-21 14:15:18 +0900

    Test : add getStoresTest

---
+++ src/main/java/com/erp/dao/StoreDAO.java
@@ -0,0 +1,11 @@
+package com.erp.dao;
+
+import com.erp.dao.dto.StoreDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface StoreDAO {
+    List<StoreDTO> getStores();
+}
