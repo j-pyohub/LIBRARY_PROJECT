612941e2f01fa1f88e659c9ad9547546f6da79d6
fuu-1 <moham18741@gmail.com>
2025-11-21 21:51:26 +0900

    feat : add StoreSalesDTO

---
+++ src/main/java/com/erp/dao/dto/StoreSalesDTO.java
@@ -0,0 +1,17 @@
+package com.erp.dao.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class StoreSalesDTO {
+    private long storeSalesNo;
+    private long storeNo;
+    private String salesDate;
+    private String salesPrice;
+}
