f1b7672685c22996bf8fa9dc24c314fd5f55725f
SeongYun <tjddbs2065@gmail.com>
2025-11-21 17:50:26 +0900

    feat: item proposal repository added

--- src/main/java/com/erp/repository/ItemProposalRepository.java
+++ src/main/java/com/erp/repository/ItemProposalRepository.java
@@ -1,6 +1,9 @@
 package com.erp.repository;
 
 import com.erp.repository.entity.ItemProposal;
+import com.erp.repository.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemProposalRepository extends JpaRepository<ItemProposal, Long> {}
+public interface ItemProposalRepository extends JpaRepository<ItemProposal, Long> {
+    Iterable<Object> findByStoreNo(Store storeNo);
+}
