9f058d5e6a8008d0c3327c869c3f9fa1dd086aab
fuu-1 <moham18741@gmail.com>
2025-11-21 17:59:46 +0900

    test : addgetManagersTest

--- src/test/java/com/erp/dao/ManagerDAOTest.java
+++ src/test/java/com/erp/dao/ManagerDAOTest.java
@@ -47,4 +47,10 @@ public class ManagerDAOTest {
         manager.setManagerId("test123");
         managerDAO.removeManager(manager);
     }
+
+    @Test
+    void getManagers() {
+        System.out.println(managerDAO.getManagers());
+
+    }
 }
