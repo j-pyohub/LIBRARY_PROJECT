9a47c794ca36263380c77f0b16428ff299f0b8a8
j-pyohub <pyosuejung@gmail.com>
2025-11-22 21:11:33 +0900

    feat: add add/setStore

--- src/main/java/com/erp/dao/StoreDAO.java
+++ src/main/java/com/erp/dao/StoreDAO.java
@@ -1,7 +1,6 @@
 package com.erp.dao;
 
 import com.erp.dao.dto.StoreDTO;
-import com.erp.dao.dto.StoreDetailDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -13,5 +12,10 @@ public interface StoreDAO {
     List<StoreDTO> getStoresByStoreName(String storeName);
     List<StoreDTO> getStoresByManagerName(String managerName);
     List<StoreDTO> getStoresByStoreStatus(String storeStatus);
-    StoreDetailDTO getStoreDetail(long storeNo);
+    StoreDTO getStoreDetail(long storeNo);
+    void addStore(StoreDTO store);
+    void setStore(StoreDTO store);
+    void setStoreRole(StoreDTO store);
+    List<StoreDTO> getStoresByAdmin();
+
 }
