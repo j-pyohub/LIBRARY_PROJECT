252ed29d5f190beca810cd4f8bbff893dc61d129
j-pyohub <pyosuejung@gmail.com>
2025-11-23 08:39:11 +0900

    test: test getSalesOrder

--- src/test/java/com/erp/repository/SalesOrderRepositoryTest.java
+++ src/test/java/com/erp/repository/SalesOrderRepositoryTest.java
@@ -1,4 +1,27 @@
 package com.erp.repository;
 
+import com.erp.repository.entity.SalesOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
 public class SalesOrderRepositoryTest {
+    @Autowired
+    private SalesOrderRepository salesOrderRepository;
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Test
+    @Transactional
+    public void getSalesOrderTest() {
+        var list = salesOrderRepository.findAll();
+        for (SalesOrder s : list) {
+            System.out.println(s.getSalesOrderNo());
+            System.out.println(s.getStore().getStoreNo());
+            System.out.println(s.getSalesOrderDatetime());
+            System.out.println(s.getSalesOrderAmount());
+        }
+    }
 }
