e0e64ae15144749ae0314235fbb0d81134a62ce8
fuu-1 <moham18741@gmail.com>
2025-11-23 00:38:21 +0900

    feat : add findBySalesDateBetween

--- src/main/java/com/erp/repository/StoreSalesRepository.java
+++ src/main/java/com/erp/repository/StoreSalesRepository.java
@@ -3,6 +3,9 @@ package com.erp.repository;
 import com.erp.repository.entity.StoreSales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreSalesRepository extends JpaRepository<StoreSales,Long> {
+import java.time.LocalDate;
+import java.util.List;
 
+public interface StoreSalesRepository extends JpaRepository<StoreSales,Long> {
+    List<StoreSales> findBySalesDateBetween(LocalDate startDate, LocalDate endDate);
 }
