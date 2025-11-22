48ebe0ddbee3a0353210b51b83bbcdacf74964fd
fuu-1 <moham18741@gmail.com>
2025-11-22 18:03:39 +0900

    fix : storeSales

--- src/main/java/com/erp/repository/StoreSalesRepository.java
+++ src/main/java/com/erp/repository/StoreSalesRepository.java
@@ -4,4 +4,5 @@ import com.erp.repository.entity.StoreSales;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreSalesRepository extends JpaRepository<StoreSales,Long> {
+
 }
