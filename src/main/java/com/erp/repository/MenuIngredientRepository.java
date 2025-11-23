0d64b459023dc48e29425df485d2fb9a405c753c
fuu-1 <moham18741@gmail.com>
2025-11-23 19:01:45 +0900

    feat : add MenuIngredient

---
+++ src/main/java/com/erp/repository/MenuIngredientRepository.java
@@ -0,0 +1,7 @@
+package com.erp.repository;
+
+import com.erp.repository.entity.MenuIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuIngredientRepository extends JpaRepository<MenuIngredient, Long> {
+}
---
+++ src/main/java/com/erp/repository/dto/MenuIngredientDTO.java
@@ -0,0 +1,10 @@
+package com.erp.repository.dto;
+
+import lombok.Data;
+
+@Data
+public class MenuIngredientDTO {
+    private Long itemNo;
+    private Long menuNo;
+    private Integer quantity;
+}
