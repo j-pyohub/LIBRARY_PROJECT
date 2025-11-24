a0ed4a0c51b2c4e6fb562b00ec6232ff1e739fa1
j-pyohub <pyosuejung@gmail.com>
2025-11-24 12:27:30 +0900

    add: addStoreMenu

--- src/main/java/com/erp/repository/entity/StoreMenu.java
+++ src/main/java/com/erp/repository/entity/StoreMenu.java
@@ -1,16 +1,14 @@
 package com.erp.repository.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class StoreMenu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
