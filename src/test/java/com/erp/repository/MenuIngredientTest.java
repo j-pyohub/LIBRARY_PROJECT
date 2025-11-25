fb4edfb913cf99e97cb7f16dce5105bb2b38fef8
fuu-1 <moham18741@gmail.com>
2025-11-25 17:56:59 +0900

    feat : add addMenuIngredientTest

--- src/test/java/com/erp/repository/MenuIngredientTest.java
+++ src/test/java/com/erp/repository/MenuIngredientTest.java
@@ -1,5 +1,7 @@
 package com.erp.repository;
 
+import com.erp.repository.entity.Item;
+import com.erp.repository.entity.Menu;
 import com.erp.repository.entity.MenuIngredient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class MenuIngredientTest {
     @Autowired
     MenuIngredientRepository menuIngredientRepository;
+
+    @Test
+    void addMenuIngredientTest(){
+        Item item = Item.builder().itemNo(43L).build();
+        Menu menu = Menu.builder().menuNo(21L).build();;
+        Integer ingredientQuantity = 50;
+        MenuIngredient newIngredient = MenuIngredient.builder().menu(menu).item(item).ingredientQuantity(ingredientQuantity).build();
+        menuIngredientRepository.save(newIngredient);
+    }
 
     @Test
     void setMenuIngredientTest() {
