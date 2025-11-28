1d506748179514a12d3b0eb3c002f43ea44de9fc
j-pyohub <pyosuejung@gmail.com>
2025-11-28 11:57:25 +0900

    refactor: advice restructure

--- src/main/java/com/erp/controller/exception/RestAdviceController.java
+++ src/main/java/com/erp/controller/exception/RestAdviceController.java
@@ -1,4 +1,4 @@
-package com.erp.controller;
+package com.erp.controller.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
